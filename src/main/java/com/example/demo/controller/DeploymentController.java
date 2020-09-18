c59bf2fed4c863edd0e79844b6bd9a76ef2d67f1
hujintao2470 <hujintao@xforceplus.com>
2020-09-18 17:48:25 +0800

    页面表格分页

--- src/main/java/com/example/demo/controller/DeploymentController.java
+++ src/main/java/com/example/demo/controller/DeploymentController.java
@@ -4,11 +4,15 @@ import com.example.demo.util.ToWeb;
 import com.example.demo.vo.DeploymentResponse;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 发布流程
@@ -16,6 +20,9 @@ import java.util.List;
 @RestController
 @RequestMapping("deployments")
 public class DeploymentController implements RestServiceController<Deployment, String> {
+
+    private Logger logger = LoggerFactory.getLogger(DeploymentController.class);
+
     @Autowired
     RepositoryService repositoryService;
 
@@ -43,6 +50,25 @@ public class DeploymentController implements RestServiceController<Deployment, S
                         .setList(list)
                         .setCurrent(page)
         );
+    }
+
+
+    @GetMapping("/list2")
+    public Object getList2(@RequestParam(value = "rowSize", defaultValue = "1000", required = false) Integer rowSize, @RequestParam(value = "page", defaultValue = "1", required = false) Integer page) {
+        logger.info("getList2 params:{} - {}", page, rowSize);
+        List<Deployment> deployments = repositoryService.createDeploymentQuery()
+                .listPage(rowSize * (page - 1), rowSize);
+        long count = repositoryService.createDeploymentQuery().count();
+        List<DeploymentResponse> list = new ArrayList<>();
+        for(Deployment deployment: deployments){
+            list.add(new DeploymentResponse(deployment));
+        }
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("total", count);
+        result.put("rows", list);
+
+        return result;
     }
 
     @Override
